534fd123c7789a8fe97f44d8e41d9ce238c6d749
Fajri <wahyuseptiadifajri11@gmail.com>
2024-09-16 16:44:03 +0700

    update the 3d model

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -6,6 +6,11 @@ import HackerRoom  from "../components/HackerRoom.jsx";
 import CanvasLoader from "../components/CanvasLoader.jsx";
 import { useMediaQuery } from 'react-responsive';
 import { calculateSizes } from '../constants/index.js';
+import Target from '../components/Target.jsx';
+import ReactLogo from '../components/ReactLogo.jsx';
+import Cube from '../components/Cube.jsx';
+import Rings from '../components/Rings.jsx';
+import HeroCamera from '../components/HeroCamera.jsx';
 
 const Hero = () => {
   const isSmall = useMediaQuery({maxWidth: 440});
@@ -16,7 +21,7 @@ const Hero = () => {
 
   return (
     <section className="min-h-screen w-full flex flex-col relative">
-      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+      <div className="w-full mx-auto flex flex-col sm:mt-35 mt-20 c-space gap-3">
          <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, i am Fajri <span className="waving-hand">👋🏻</span></p>
          <p className="hero_tag text-gray_gradient">Man Of Culture As Well</p>
       </div>
@@ -26,11 +31,19 @@ const Hero = () => {
           <Suspense fallback={<CanvasLoader />}>  
 
           <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+          <HeroCamera isMobile={isMobile}>
           <HackerRoom 
           position={sizes.deskPosition} 
           scale={sizes.deskScale} 
           rotation={[0, -Math.PI, 0]} 
           />
+          </HeroCamera>
+          <group>
+            <Target position={sizes.targetPosition} />
+            <ReactLogo position={sizes.reactLogoPosition}/>
+            <Cube position={sizes.cubePosition}/>
+            <Rings position={sizes.ringPosition}/>
+          </group>
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
           </Suspense>
