12c319693782eafe1588636309c72db9ed216c66
Fajri <wahyuseptiadifajri11@gmail.com>
2024-09-24 17:50:47 +0700

    finishing my portfolio

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,90 @@
+import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser'
+import Swal from 'sweetalert2'
+
+const Contact = () => {
+  const formRef = useRef()
+
+  const [loading, setLoading] = useState(false)
+
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+
+  const handleChange = ({target: {name, value}}) => {
+    setForm({...form, [name]: value})
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true)
+    try {
+      await emailjs.send('service_iq8cptu', 'template_0pqozhw', {
+        from_name: form.name,
+        to_name: 'Fajri',
+        from_email: form.email,
+        to_email: 'wahyuseptiadifajri11@gmail.com',
+        message: form.message
+      },
+      'xFkUO259tbvcMgz3b'
+    )
+      setLoading(false)
+      // alert('Your Message Has Been Sent')
+      Swal.fire({
+        title: "Thanks!",
+        text: "Your message has been sent",
+        icon: "success"
+      });
+      setForm({
+        name: '',
+        email: '',
+        message: ''
+      })
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+      // alert('Something Went Wrong')
+      Swal.fire({
+        title: "Oops!",
+        text: "Something went wrong",
+        icon: "error"
+      });
+    }
+
+  }
+
+
+  return (
+    <section className="c-space my-20" id="contact">
+      <div className="relative min-h-screen flex items-center justify-center flex-col">
+        <img src="/assets/terminal.png" alt="terminalbackround" className="absolute inset-0 min-h-screen" />
+        <div className="contact-container">
+          <h3 className="head-text">Let's Talk</h3>
+          <p className='text-lg text-white-600 mt-3'>Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to life, I’m here to help.</p>
+
+          <form ref={formRef} onSubmit={handleSubmit} className='mt-12 flex flex-col space-y-7'>
+            <label className="space-y-3">
+              <span className='field-label'>Full Name</span>
+              <input type="text" name="name" value={form.name} onChange={handleChange} required className='field-input' placeholder='Megumi Kato' />
+            </label>
+            <label className="space-y-3">
+              <span className='field-label'>Email</span>
+              <input type="email" name="email" value={form.email} onChange={handleChange} required className='field-input' placeholder='megumikato@gmail.com' />
+            </label>
+            <label className="space-y-3">
+              <span className='field-label'>Your Message</span>
+              <textarea name="message" value={form.message} onChange={handleChange} required rows={5} className='field-input' placeholder="Hi, I'm interested in ..." />
+            </label>
+            <button className='field-btn' type='submit' disabled={loading}>
+              {loading ? 'Sending ...' : 'Send Message'}
+              <img src="/assets/arrow-up.png" alt="arrow-up" className='field-btn_arrow' />
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
